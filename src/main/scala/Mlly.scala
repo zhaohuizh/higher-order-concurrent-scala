0efea8e64b33f8a5d00b06b177fbf4c08dd5ee6b
PLTDon <konstantinos.pouliasis@gmail.com>
2014-05-03 17:25:13 -0400

    fixing comprehension syntax but getting errors

--- src/main/scala/Mlly.scala
+++ src/main/scala/Mlly.scala
@@ -4,12 +4,14 @@
 
 import scalaz.concurrent.MVar
 import scalaz.concurrent.MVar._
+import scalaz.concurrent.Strategy
 import scalaz.effect._
 import IO._
+import concurrent._
 import scalaz.OptionT
 import scalaz.ListT
 
-object MyConcMl{
+object Mlly{
   type Commit = MVar[Boolean]
   type Decision = MVar[Option[Commit]]
   type Candidate = MVar[Decision]
@@ -21,34 +23,33 @@ object MyConcMl{
   type Synchronizer = MVar[Pair[Point, Decision]]
 
   type Event[T] = Synchronizer => Abort => Name => IO[T]
-
+  //type Channel[T]=(In,Out,MVar[T])
   def maybe[A, B](b: => B)(f: A => B)(opt: Option[A]): B = {
     opt.map(f).getOrElse(b)
   }
 
-  def atchannel(i: In, o: Out): IO[Unit] = for {
-    (ei: Candidate) <- i take;
-    (eo: Candidate) <- o take;
-    ( si: Decision) <- newEmptyMVar[Option[Commit]];
-    _ <- ei.put(si)
-    ki: Option[Commit] <- si take;
-      (so: Decision) <- newEmptyMVar[Option[Commit]]
-      _ <- eo put (so)
-      ko: Option[Commit] <- so take
-  }
-    yield {
-      maybe(ioUnit)((ci: Commit) => for {_ <- ci.put(ko isDefined)} yield {})(ki)
-      maybe(ioUnit)((ci: Commit) => for {_ <- ci.put(ko isDefined)} yield {})(ki)
+  def forkIO(f: => IO[Unit])(implicit s: Strategy): IO[Unit] =
+    IO { s(f.unsafePerformIO); () }
+
+  def atchannel(i: In, o: Out): IO[Unit] =
+    for(
+      ei <- i.take;
+      eo <- o.take;
+      si <- newEmptyMVar[Option[Commit]];
+      yy <- ei.put(si);
+      ki <- si take;
+      so <- newEmptyMVar[Option[Commit]];
+      xx <- eo.put(so);
+      ko <- so take
+     // _ <- maybe(ioUnit)((ci: Commit) => for (_ <- ci.put(ko isDefined)) yield {()})(ki)
+
+    ) yield {
+
+     // maybe(ioUnit)((ci: Commit) => for (_ <- ci.put(ko isDefined)) yield {})(ki);
+      //maybe(ioUnit)((ci: Commit) => for (_ <- ci.put(ko isDefined)) yield {})(ki)
     }
 
 
-
-
-  def atsync(r: Synchronizer)(a: Abort)(x: IO[Unit]): IO[Unit] =
-    for {(t, s) <- r take
-
-
-    } yield {}
 
 
 }
